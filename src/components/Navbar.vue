950e5c46d0c812d5bfd8c8d373bc3654e7adc318
Roman Matsypula <ukrmatsypula@gmail.com>
2021-06-23 16:43:00 +0300

    init and added nav component

---
+++ src/components/Navbar.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <nav class="blue darken-3">
+      <div class="nav-wrapper">
+        <router-link to="/" class="brand-logo">Tasks</router-link>
+        <ul class="right hide-on-med-and-down">
+          <router-link to="/" tag="li" exact active-class="active"
+            ><a href="#">Create</a>
+          </router-link>
+          <router-link to="/list" tag="li" active-class="active"
+            ><a href="#">List</a>
+          </router-link>
+        </ul>
+      </div>
+    </nav>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+nav {
+  padding: 0 2rem;
+}
+</style>
