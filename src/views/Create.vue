a8f7289d9aed409ca3c54d9c7daba322ccdf2b67
Roman Matsypula <ukrmatsypula@gmail.com>
2021-06-24 13:30:25 +0300

    added field input

--- src/views/Create.vue
+++ src/views/Create.vue
@@ -1,12 +1,22 @@
 <template>
-  <div>
-    <h1>Create</h1>
+  <div class="row">
+    <div class="col s6 offset-s3">
+      <div class="input-field">
+        <h1>Create task</h1>
+        <form>
+          <div class="input-field">
+            <input id="title" type="text" class="validate" required />
+            <label for="title">Title</label>
+          </div>
+        </form>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
 export default {
-  name: "Home",
+  name: "Create",
   components: {},
 };
 </script>
