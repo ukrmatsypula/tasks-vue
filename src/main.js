ff0d66a0e746edd3bd8bda59ac86265852eebea4
Roman Matsypula <ukrmatsypula@gmail.com>
2021-06-24 15:36:09 +0300

    added fonts material

--- src/main.js
+++ src/main.js
@@ -3,7 +3,7 @@ import App from "./App.vue";
 import "./registerServiceWorker";
 import router from "./router";
 import store from "./store";
-import "materialize-css/dist/js/materialize.min";
+import "materialize-css";
 
 Vue.config.productionTip = false;
 
--- src/views/Create.vue
+++ src/views/Create.vue
@@ -8,6 +8,7 @@
             <input id="title" type="text" class="validate" required />
             <label for="title">Title</label>
           </div>
+          <div class="chips" ref="chips"></div>
         </form>
       </div>
     </div>
@@ -15,8 +16,14 @@
 </template>
 
 <script>
+import M from "materialize-css";
 export default {
   name: "Create",
   components: {},
+  mounted() {
+    M.Chips.init(this.$refs.chips, {
+      placeholder: "Task tags",
+    });
+  },
 };
 </script>
