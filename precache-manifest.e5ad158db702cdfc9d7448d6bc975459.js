751ccee058120f6a0e21e1596fccb5e9f7a23b14
Roman Matsypula <ukrmatsypula@gmail.com>
2021-06-25 13:18:04 +0300

    deploy

---
+++ precache-manifest.e5ad158db702cdfc9d7448d6bc975459.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7daab3a1e26a9f47d5b3",
+    "url": "/tasks-vue/css/app.3d5ef3a8.css"
+  },
+  {
+    "revision": "ce0c8b3dec0311de906c",
+    "url": "/tasks-vue/css/chunk-61ffbd88.e5565c20.css"
+  },
+  {
+    "revision": "01e8a3ea82f11c4a70f4",
+    "url": "/tasks-vue/css/chunk-vendors.24ce4a6c.css"
+  },
+  {
+    "revision": "af3adefcb9ee9bc8f60e2ad983202ae9",
+    "url": "/tasks-vue/index.html"
+  },
+  {
+    "revision": "7daab3a1e26a9f47d5b3",
+    "url": "/tasks-vue/js/app.0d2651b9.js"
+  },
+  {
+    "revision": "59229db21d0ebd9ad744",
+    "url": "/tasks-vue/js/chunk-2d0b64c8.7c31627f.js"
+  },
+  {
+    "revision": "e5a0f2c38a1685b60f5d",
+    "url": "/tasks-vue/js/chunk-2d21ef2c.27f2cd0a.js"
+  },
+  {
+    "revision": "ce0c8b3dec0311de906c",
+    "url": "/tasks-vue/js/chunk-61ffbd88.14ea3603.js"
+  },
+  {
+    "revision": "01e8a3ea82f11c4a70f4",
+    "url": "/tasks-vue/js/chunk-vendors.9091e434.js"
+  },
+  {
+    "revision": "bc2d8f784a307f88a1e98a711da2ec9f",
+    "url": "/tasks-vue/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/tasks-vue/robots.txt"
+  }
+]);
